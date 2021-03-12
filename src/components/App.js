81dde10f68f90d4f108b1190dcb515100f06ceb9
sahu829 <sahukrishna829@gmail.com>
2021-03-12 08:25:50 +0530

    Change

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,33 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+    this.state = { paragraph: "" };
+  }
+
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        <paragraph id="para">{this.state.paragraph}</paragraph>
+
+        <button
+          id="click"
+          onClick={() => {
+            this.setState({
+              paragraph:
+                "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+            });
+          }}
+        >
+          btn
+        </button>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
